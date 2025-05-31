32d34f0f0e50ff672d2404ecd67788192709bc9c
kay1403 <angekoumba754@gmail.com>
2025-05-31 13:44:52 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
-"""Utils module"""
-
-from typing import Mapping, Sequence, Any, Dict
+"""Utils module: access_nested_map, get_json, and memoize."""
+from typing import Mapping, Any, Sequence
 import requests
+from functools import wraps
 
 
 def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
-    """Access value in nested map using a sequence of keys"""
+    """Access value in nested map using a sequence of keys."""
     for key in path:
         nested_map = nested_map[key]
     return nested_map
 
 
-def get_json(url: str) -> Dict:
-    """Get JSON response from URL"""
+def get_json(url: str) -> Any:
+    """Get JSON content from a URL."""
     response = requests.get(url)
     return response.json()
 
 
 def memoize(method):
-    """Memoization decorator"""
+    """Decorator to cache the result of a method."""
     attr_name = f"_memoized_{method.__name__}"
 
+    @wraps(method)
     def wrapper(self):
         if not hasattr(self, attr_name):
             setattr(self, attr_name, method(self))
