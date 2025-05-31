3d3fff6aaa2d598d7e20deecc15c3dabc9ad262d
kay1403 <angekoumba754@gmail.com>
2025-05-31 12:43:37 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python3
-""" Utility functions for testing """
+"""Utilities module"""
 
-def access_nested_map(nested_map, path):
-    """Access a nested map with a list of keys"""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
+import requests
 
 
 def get_json(url):
-    """Mocked function for getting JSON from a URL"""
-    import requests
+    """Get JSON from URL"""
     response = requests.get(url)
     return response.json()
-
-
-def memoize(method):
-    """Memoize decorator"""
-    attr_name = "_{}".format(method.__name__)
-
-    def wrapper(self):
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, method(self))
-        return getattr(self, attr_name)
-    return wrapper
