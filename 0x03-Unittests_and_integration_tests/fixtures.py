1e22ee20ea3f3a82656c4ceb1fb2f40048f33f08
kay1403 <angekoumba754@gmail.com>
2025-05-31 12:44:26 +0200

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,39 +1,22 @@
 #!/usr/bin/env python3
-""" Fixture module for client unit tests """
+"""Fixtures for testing"""
 
 TEST_PAYLOAD = {
-    "org": {
-        "login": "google",
-        "id": 1342004,
-        "repos_url": "https://api.github.com/orgs/google/repos"
-    },
     "repos": [
         {
-            "id": 7697149,
+            "id": 1,
             "name": "dagger",
-            "license": {
-                "key": "apache-2.0",
-                "name": "Apache License 2.0",
-                "spdx_id": "Apache-2.0"
-            }
+            "license": {"key": "apache-2.0"}
         },
         {
-            "id": 7776515,
+            "id": 2,
             "name": "flatbuffers",
-            "license": {
-                "key": "apache-2.0",
-                "name": "Apache License 2.0",
-                "spdx_id": "Apache-2.0"
-            }
+            "license": {"key": "mit"}
         },
         {
-            "id": 7787085,
+            "id": 3,
             "name": "volley",
-            "license": {
-                "key": "other",
-                "name": "Other",
-                "spdx_id": "OTHER"
-            }
+            "license": {"key": "bsd-3-clause"}
         }
     ]
 }
