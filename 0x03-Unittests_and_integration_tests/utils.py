9d79e1bb75f28ca3fe673f821d4a0e6c1120e9e3
kay1403 <angekoumba754@gmail.com>
2025-05-31 13:23:35 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
-"""Utilities module"""
+"""Utils module"""
 
+from typing import Mapping, Sequence, Any, Dict
 import requests
 
 
-def get_json(url):
-    """Get JSON from URL"""
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access value in nested map using a sequence of keys"""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
+
+
+def get_json(url: str) -> Dict:
+    """Get JSON response from URL"""
     response = requests.get(url)
     return response.json()
+
+
+def memoize(method):
+    """Memoization decorator"""
+    attr_name = f"_memoized_{method.__name__}"
+
+    def wrapper(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, method(self))
+        return getattr(self, attr_name)
+
+    return wrapper
