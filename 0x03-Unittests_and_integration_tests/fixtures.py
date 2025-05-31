a5487d3d70eefbef3a6b7275100fce6c3e59fed4
kay1403 <angekoumba754@gmail.com>
2025-05-31 13:45:28 +0200

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
-"""Test fixtures for GithubOrgClient"""
+"""Fixtures for integration tests"""
 
-TEST_PAYLOAD = {
-    "org_payload": {
-        "login": "google",
-        "id": 1,
-        "repos_url": "https://api.github.com/orgs/google/repos"
-    },
-    "repos_payload": [
-        {
-            "id": 1,
-            "name": "repo1",
-            "license": {
-                "key": "apache-2.0"
-            }
-        },
-        {
-            "id": 2,
-            "name": "repo2",
-            "license": {
-                "key": "other"
-            }
-        }
-    ],
-    "expected_repos": ["repo1", "repo2"],
-    "apache2_repos": ["repo1"]
-}
+TEST_PAYLOAD = [
+    (
+        {"login": "google"},
+        [
+            {"name": "repo1", "license": {"key": "apache-2.0"}},
+            {"name": "repo2", "license": {"key": "mit"}},
+            {"name": "repo3", "license": {"key": "apache-2.0"}}
+        ],
+        ["repo1", "repo2", "repo3"],
+        ["repo1", "repo3"]
+    )
+]
