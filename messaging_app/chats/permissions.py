8e40e62aab23981b5df7a0dc7167319bc40eacac
kay1403 <angekoumba754@gmail.com>
2025-06-08 11:56:41 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsParticipantOfConversation(BasePermission):
-    """
-    Only allow users who are participants of the conversation to access it.
-    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated  # ✅ Auth check
 
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
+        if request.method in SAFE_METHODS or request.method in ["PUT", "PATCH", "DELETE"]:  # ✅ méthode check
+            return request.user in obj.participants.all()  # ✅ participant check
+        return False
