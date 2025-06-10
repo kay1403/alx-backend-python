0858bbfaba8d5b1a5782098ab29bc8afc7a3ba0e
kay1403 <angekoumba754@gmail.com>
2025-06-10 21:59:36 +0200

    Update urls.py

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import message_history_view
+from .views import message_history_view, MessageHistoryAPIView
 
 urlpatterns = [
+    # Vue HTML
     path('messages/<int:message_id>/history/', message_history_view, name='message_history'),
+
+    # API JSON
+    path('api/messages/<int:message_id>/history/', MessageHistoryAPIView.as_view(), name='message_history_api'),
 ]
