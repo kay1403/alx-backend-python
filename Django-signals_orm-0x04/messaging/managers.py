00aa46df2ae8d9f36dcfefb19084a411996b85ac
kay1403 <angekoumba754@gmail.com>
2025-06-10 22:46:56 +0200

    Update managers.py

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class UnreadMessagesManager(models.Manager):
-    def for_user(self, user):
+    def unread_for_user(self, user):
+        # Ne récupérer que les champs nécessaires avec .only()
         return self.filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
