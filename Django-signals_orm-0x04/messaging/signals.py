9fe3232dcd6565d6fee5fa51f8bb8e094ed72f33
kay1403 <angekoumba754@gmail.com>
2025-06-10 23:11:51 +0200

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
@@ -15,3 +15,32 @@ def delete_related_user_data(sender, instance, **kwargs):
     # Supprimer l'historique des messages liés
     MessageHistory.objects.filter(message__sender=instance).delete()
     MessageHistory.objects.filter(message__receiver=instance).delete()
+
+@receiver(post_save, sender=Message)
+def create_notification_on_new_message(sender, instance, created, **kwargs):
+    if created:
+        # Crée une notification pour le destinataire
+        Notification.objects.create(user=instance.receiver, message=instance)
+
+@receiver(pre_save, sender=Message)
+def log_message_edit_history(sender, instance, **kwargs):
+    if not instance.pk:
+        # Nouveau message, rien à faire ici
+        return
+
+    try:
+        old_instance = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        return
+
+    # Si le contenu a changé, sauvegarder l'historique
+    if old_instance.content != instance.content:
+        MessageHistory.objects.create(
+            message=instance,
+            old_content=old_instance.content,
+            edited_by=instance.edited_by
+        )
+        # Mettre à jour les champs d'édition du message
+        instance.edited = True
+        from django.utils import timezone
+        instance.edited_at = timezone.now()
